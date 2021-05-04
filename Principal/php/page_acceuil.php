15b06f798a022a3b33f66ca7e3156ebc13dcd64c
Rafii-Ayoub <72503025+Rafii-Ayoub@users.noreply.github.com>
2021-05-04 19:14:31 +0200

    Add files via upload

---
+++ Principal/php/page_acceuil.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel = "stylesheet" media="screen and (min-width: 1660px)" href="page_accueil.css">
+	<link rel="stylesheet" media="screen and (max-width: 1659px)" href="page_accueil2.css">
+	<title>Gestion des oraux</title>
+</head>
+
+<body>
+	<div id="body">
+		<div id="sub_menu">
+		<div id="logo">
+			<ul id="element">
+					<li>
+						<a href="page_acceuil.html" class="logo"><b Style="color:white;font-size:30px; ">POLYTECH <span Style="color:#33FFBC;">ANNECY</span></b></a>
+					</li>
+					<li>
+						<img src ="Logo_USMB_blanc_web_RVB.png" alt ="polytech logo"> 
+					</li>
+				</ul>
+		</div>
+		
+		<div id = "title">
+			<p style="color: white;">GESTION DES ENTRETIENS</P>
+		</div>
+
+		</div>
+		
+		<div id="contenu">
+		<div id="text">
+			<p style="font-size:40px;">Bienvenue sur le site de gestion des oraux<p>
+			<div id="info">
+				<p style="font-size:17px;">
+				Il est rappelé aux candidats que les normes de distanciation sociale doivent être respectées et que le port du masque est obligatoire en permanence, y compris pendant les épreuves.<br><br>
+				La conduite à tenir lorsque l’on est positif, symptomatique ou asymptomatique ou cas contact, est fixée par directive gouvernementale. Elle engage la responsabilité individuelle.
+				Un candidat positif ou cas contact ne pourra pas être autorisé à concourir. Le risque sanitaire pour les autres candidats, les surveillants et le personnel de l'organisation des
+				centres d'écrit ne le permet pas.<br><br>
+				En conséquence, il est fortement recommandé aux candidats d’éviter les rassemblements et les conduites à risque pendant la période précédant les épreuves et durant toute la période des épreuves.
+				</p>
+			</div>
+			<p style="font-size:20px;">Vous êtes?</p>
+		</div>
+		
+		<div id="button">
+			<button id="poste"><a href="login.php">Etudiant</a></button>
+			<button id="poste"><a href="login_ens.php">Enseignant</a></button>
+			<button id="poste"><a href="login_adm.php">Administrateur</a></button>
+		</div>
+		</div>
+		
+	</div>
+	<div id="sub_body">
+		<div class="col-md-3 widget">
+			<h3 class="widget-title">Contact</h3>
+			<div class="widget-body">			
+				<p><a <a href="mailto:admission@polytech-annecy-chambery.fr" target="_top" Style="color:#33FFBC;">
+					Nous écrire</a></p>
+				<p>Tel: +33(0)450 096 600</p>
+				<p>Polytech Annecy-Chambéry<br/>
+					5 chemin Bellevue / Annecy-le-Vieux / 74940 ANNECY / FRANCE	
+				</p>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
